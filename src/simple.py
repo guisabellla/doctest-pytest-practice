a4dee4394c6f3da218f6d10f82807b22cafbe253
guisabellla <guisabella05@hotmail.com>
2026-09-17 20:08:45 -0700

    modified simple

--- src/simple.py
+++ src/simple.py
@@ -5,6 +5,7 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
@@ -20,11 +21,13 @@ def evens(n):
     >>> evens(-1)
     []
     '''
-    return list(range(0, n + 1, 2))
+    return list(filter(lambda x: x % 2 == 0, range(n + 1)))
+
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n inclusive
+    that contain the digit 3.
 
     >>> threes(2)
     []
@@ -37,19 +40,17 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
-    accumulator = []
-    for x in range(n + 1):
-        if '3' in str(x):
-            accumulator.append(x)
-    return accumulator
+    return list(filter(lambda x: '3' in str(x), range(n + 1)))
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 5 characters long.
+    Returns a list of all words in the input text that
+    are less than 5 characters long.
 
     HINT:
-    Recall that text.split() converts the text variable into a list of words.
+    Recall that text.split() converts the text variable
+    into a list of words.
 
     >>> small_words('this is a simple test case')
     ['this', 'is', 'a', 'test', 'case']
@@ -60,17 +61,15 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
-    accumulator = []
-    for word in text.split():
-        if len(word) < 5:
-            accumulator.append(word)
-    return accumulator
+    return list(filter(lambda word: len(word) < 5, text.split()))
 
 
 def squares(n):
     '''
-    Returns a list of all square number between 1 and n inclusive.
-    Recall that the nth square number is defined to be n*n.
+    Returns a list of all square number
+    between 1 and n inclusive.
+    Recall that the nth square number is
+    defined to be n*n.
 
     >>> squares(1)
     [1]
@@ -81,15 +80,13 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
-    accumulator = []
-    for x in range(1, n + 1):
-        accumulator.append(x * x)
-    return accumulator
+    return list(map(lambda x: x * x, range(1, n + 1)))
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given a list of strings, returns a list of the
+    lengths of the corresponding strings.
 
     >>> lengths([])
     []
@@ -98,7 +95,4 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
-    accumulator = []
-    for string in strings:
-        accumulator.append(len(string))
-    return accumulator
+    return list(map(len, strings))
